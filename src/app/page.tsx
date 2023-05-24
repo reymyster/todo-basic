0d2bb43989b5e0311d7426547fafcb93c198bc9a
Rey Trajano <rey@reytrajano.com>
2023-05-24 16:05:11 -0700

    initial add form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,42 @@
 export default function Home() {
   return (
-    <main className="mx-auto max-w-4xl px-4 py-12 sm:px-6 lg:px-8">
-      <h1 className="py-4 text-center text-3xl">TODOS</h1>
+    <main className="mx-auto max-w-2xl px-4 py-12 sm:px-6 lg:px-8">
+      <h1 className="py-4 text-center text-3xl">TO DO&apos;S</h1>
+      <AddForm />
     </main>
+  );
+}
+
+function AddForm() {
+  async function addItem(formData: FormData) {
+    "use server";
+    const newTodo = formData.get("todo");
+    console.log({ newTodo });
+  }
+
+  return (
+    <form action={addItem}>
+      <div className="flex flex-row justify-between">
+        <div className="grow p-2">
+          <input
+            className="form-input w-full rounded-md text-black"
+            type="text"
+            id="todo"
+            name="todo"
+            placeholder="To do..."
+            maxLength={250}
+            required={true}
+          />
+        </div>
+        <div className="flex grow-0 content-center justify-center p-2">
+          <button
+            type="submit"
+            className="rounded-md bg-white/80 p-1 px-4 text-black/80 duration-75 hover:bg-white hover:text-black"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </form>
   );
 }
