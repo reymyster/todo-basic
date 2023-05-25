38ffd0097769e71bd553f9c876601c388ea782e4
Rey Trajano <rey@reytrajano.com>
2023-05-24 18:45:25 -0700

    implement set completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,36 @@
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 async function getData() {
   const { rows } =
     await sql`SELECT id, title, completed FROM todos WHERE completed = ${0} ORDER BY id ASC`;
 
-  return rows.map((r) => ({
-    id: r.id as number,
-    title: r.title as string,
-    completed: r.completed === "1",
-  }));
+  return rows.map(
+    (r) =>
+      ({
+        id: r.id as number,
+        title: r.title as string,
+        completed: r.completed === "1",
+      } as Todo)
+  );
 }
 
 export default async function Home() {
   const data = await getData();
+  console.log({ data });
 
   return (
     <main className="mx-auto max-w-2xl px-4 py-12 sm:px-6 lg:px-8">
       <h1 className="py-4 text-center text-3xl">TO DO&apos;S</h1>
       <AddForm />
       <hr className="my-4" />
-      <div className="px-8">
-        <ul className="list-disc">
-          {data.map((item) => (
-            <li key={item.id} className="p-2">
-              {item.title}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <DisplayTodos data={data} />
     </main>
   );
 }
@@ -65,5 +67,44 @@ function AddForm() {
         </div>
       </div>
     </form>
+  );
+}
+
+function DisplayTodos({ data }: { data: Todo[] }) {
+  return (
+    <div className="px-1">
+      <ul>
+        {data.map((item, index) => (
+          <DisplayTodo key={item.id} index={index} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function DisplayTodo({ item, index }: { item: Todo; index: number }) {
+  const { id } = item;
+  async function setCompleted() {
+    "use server";
+    await sql`UPDATE todos SET completed = '1' WHERE id = ${id}`;
+    revalidatePath("/");
+  }
+
+  return (
+    <li
+      className={`flex justify-between p-2 ${
+        index % 2 === 1 ? "bg-white/20" : ""
+      }`}
+    >
+      <span>{item.title}</span>
+      <form action={setCompleted}>
+        <button
+          type="submit"
+          className="rounded-md bg-white/80 px-4 py-1 text-black/80 duration-75 hover:bg-white hover:text-black"
+        >
+          Complete
+        </button>
+      </form>
+    </li>
   );
 }
